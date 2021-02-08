614f63c464204316c564b66625c0e54e456f7787
NathanLeakeas <ncleakeas@stu.naperville203.org>
2021-02-08 12:41:13 -0600

    Player look is now locked between -90 and 90 degrees

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed=1;
     public float sprintSpeedMult=1.25f;
-    public float verticalSensitivity = 1;
+    public float verticalSensitivity = 2;
     public float horizontalSensitivity = 1;
     private float camRotationX;
     private CharacterController character;
@@ -16,7 +16,6 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         character = GetComponent<CharacterController>();
-        //camRotationX = cam.transform.eulerAngles.x;
     }
 
     // Update is called once per frame
@@ -29,23 +28,15 @@ public class PlayerController : MonoBehaviour
         Vector3 horizontalRotation = new Vector3(0, Input.GetAxis("Mouse X") * horizontalSensitivity, 0);
         transform.Rotate(horizontalRotation);
 
-        //Cam limit attempt: Restrict via manual angle input/clamping
-        /*float verticalRotation = -Input.GetAxis("Mouse Y")*verticalSensitivity;
-        camRotationX = Mathf.Clamp(camRotationX,90f,-90f);
-        cam.transform.rotation = Quaternion.Euler(camRotationX,cam.transform.rotation.eulerAngles.y, cam.transform.rotation.eulerAngles.z);*/
 
-        //Cam Limit attempt: Restrict via if statement
-        Vector3 verticalRotation = new Vector3(-Input.GetAxis("Mouse Y")*verticalSensitivity, 0, 0);
-        //Debug.Log("Camera X Rotation: " + cam.transform.localEulerAngles.x);
-        /*if ((cam.transform.localEulerAngles.x >=0 && cam.transform.localEulerAngles.x<=90)|| (cam.transform.localEulerAngles.x >= 270 && cam.transform.localEulerAngles.x <= 360))
-        {
-            Debug.Log("Should stop rotation");*/
-            cam.transform.Rotate(verticalRotation);
-        //}
-        
+        camRotationX -= Input.GetAxis("Mouse Y") * verticalSensitivity;
+        camRotationX = Mathf.Clamp(camRotationX, -89f, 89f);
+        Debug.Log(camRotationX);
+        cam.transform.eulerAngles = new Vector3(camRotationX, transform.eulerAngles.y, 0.0f);
 
-        //jump code here
+
 
 
     }
+
 }
