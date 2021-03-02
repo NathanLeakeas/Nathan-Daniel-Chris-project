909f3c907c3b38cc0a22bde794beeee6b393eb04
NathanLeakeas <ncleakeas@stu.naperville203.org>
2021-03-02 13:12:31 -0600

    Enabled mouse locking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,6 +26,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         if (character.isGrounded)
         {
             yVelocity = 0;
