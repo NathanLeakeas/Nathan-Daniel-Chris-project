888bfcd1b65f3486bd80b02ce73c4d312e888d7b
NathanLeakeas <ncleakeas@stu.naperville203.org>
2021-04-28 12:12:40 -0500

    Missing semicolon

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Waypoint : MonoBehaviour
 {
     [SerializeField]
-    protected float debugDrawRadius = 1.0f
+    protected float debugDrawRadius = 1.0f;
     
     public virtual void OnDrawGizmos()
     {
